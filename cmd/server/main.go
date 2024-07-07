759c641edb3ea5c8d432a468b1be33e3d8860634
“xc” <2141682582@qq.com>
2024-07-07 22:33:16 +0800

    Merge remote-tracking branch 'origin/main'

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("web/static/*.html")
-	router.GET("", func(c *gin.Context) { c.HTML(200, "index.html", nil) })
+	router.Static("static", "web/static")
+	v := router.Group("/")
+	v.GET("/", func(c *gin.Context) { c.HTML(200, "index.html", nil) })
 	router.Run(":8080")
 }
